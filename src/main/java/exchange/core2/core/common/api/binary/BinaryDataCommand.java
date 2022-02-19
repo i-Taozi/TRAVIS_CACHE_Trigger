2d95be414a9334af423535076c6086c9b2576384
i-Taozi <879714748@qq.com>
2022-02-19 21:14:45 +0800

    nacos-group/nacos-spring-boot-project:original code

--- src/main/java/exchange/core2/core/common/api/binary/BinaryDataCommand.java
+++
@@ -1,9 +0,0 @@
-package exchange.core2.core.common.api.binary;
-
-import net.openhft.chronicle.bytes.WriteBytesMarshallable;
-
-public interface BinaryDataCommand extends WriteBytesMarshallable {
-
-    int getBinaryCommandTypeCode();
-
-}
